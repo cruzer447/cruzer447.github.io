5bb210028579043c13e959fdd672ae38fe95493f
Shreyansh Misra <misrashreyansh6@gmail.com>
2026-07-01 04:45:10 +0000

    ReInvent

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Portfolio loaded 🚀");
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link => {
+
+    link.addEventListener("click", function () {
+
+        links.forEach(l => l.classList.remove("active"));
+
+        this.classList.add("active");
+
+    });
+
+});
+
+window.addEventListener("scroll", () => {
+
+    const nav = document.querySelector("nav");
+
+    if(window.scrollY > 50){
+
+        nav.style.boxShadow = "0 5px 20px rgba(0,0,0,.4)";
+
+    }
+
+    else{
+
+        nav.style.boxShadow = "none";
+
+    }
+
+});
